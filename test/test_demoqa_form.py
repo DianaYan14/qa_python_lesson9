254aa3564240552d2a09df72539c52ab3100d4ab
Diana Yanysheva <dintos.pisos@gmail.com>
2023-05-06 22:15:36 +0500

    refactoring for test

--- test/test_demoqa_form.py
+++ test/test_demoqa_form.py
@@ -1,41 +1,30 @@
-from selene import browser, be, have
-import os
+from qa_python_lesson9.demoqa_form import RegistrationPage
+from selene import have
 
 
 def test_send_form(browser_go):
+    registration_page = RegistrationPage()
     # открыть форму
-    browser.open('/automation-practice-form')
-    browser.element('.practice-form-wrapper').should(have.text('Student Registration Form'))
+    registration_page.open()
     # заполнить форму
-    browser.element("#firstName").type('Sherlock')
-    browser.element("#lastName").type('Holmes')
-    browser.element("#userEmail").type('HolmsTest@gmail.com')
-    browser.element("[value=Male]").double_click()
-    browser.element("#userNumber").type('8977777575')
-    browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker-popper').should(be.visible)
-    browser.element('.react-datepicker__year-select').click()
-    browser.element('[value="1984"]').click()
-    browser.element('.react-datepicker__month-select').click()
-    browser.element('[value="0"]').click()
-    browser.element('.react-datepicker__day--006').should(have.attribute('aria-label', 'Choose Friday, January 6th, 1984')).click()
-    browser.element('#dateOfBirthInput').should(have.value('06 Jan 1984'))
-    browser.element("#subjectsInput").type('Maths').press_enter()
-    browser.element("#subjectsInput").type('Eng').press_enter()
-    browser.element('label[for="hobbies-checkbox-2"]').click()
-    browser.element('#uploadPicture').send_keys(os.getcwd() + '/picture.jpg')
-    browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
-    browser.element('#currentAddress').type('221B Baker Street')
-    browser.element('#state').click()
-    browser.element('#react-select-3-input').set_value('NCR').press_tab()
-    browser.element('#react-select-4-input').type('Delhi').press_enter()
+    registration_page.fill_first_name('Sherlock')
+    registration_page.fill_last_name('Holmes')
+    registration_page.fill_email('HolmsTest@gmail.com')
+    registration_page.choose('Male')
+    registration_page.fill_number('8977777575')
+    registration_page.fill_date_of_Birth('January 6th, 1984')
+    registration_page.assert_date_of_Birth().should(have.value('06 Jan 1984'))
+    registration_page.select_subjects('Maths', 'Eng')
+    registration_page.set_checkbox('2')
+    registration_page.upload_picture('picture.jpg')
+    registration_page.fill_address('221B Baker Street')
+    registration_page.select_state_and_city('NCR', 'Delhi')
     # отправить форму
-    browser.element("footer").execute_script('element.remove()')
-    browser.element('#submit').execute_script('element.click()')
-    browser.element('.modal-title').should(have.text('Thanks for submitting the form'))
-    browser.all('tbody tr').should(have.exact_texts(
-        'Student Name Sherlock Holmes', 'Student Email HolmsTest@gmail.com', 'Gender Male', 'Mobile 8977777575',
-        'Date of Birth 06 January,1984', 'Subjects Maths, English', 'Hobbies Reading',
-        'Picture picture.jpg', 'Address 221B Baker Street',
-        'State and City NCR Delhi'))
-    browser.element('#closeLargeModal').click()
+    registration_page.send_form().should(have.text('Thanks for submitting the form'))
+    registration_page.user_info().should(
+        have.exact_texts('Student Name Sherlock Holmes', 'Student Email HolmsTest@gmail.com', 'Gender Male',
+                         'Mobile 8977777575',
+                         'Date of Birth 06 January,1984', 'Subjects Maths, English', 'Hobbies Reading',
+                         'Picture picture.jpg', 'Address 221B Baker Street',
+                         'State and City NCR Delhi'))
+    registration_page.close_modal()
